37c1a989fde72398f123ba8ab07a7da69b2839b4
lequito <=>
2022-01-24 19:45:23 -0300

    update para categorias

--- src/app/components/views/categoria/categoria.service.ts
+++ src/app/components/views/categoria/categoria.service.ts
@@ -35,6 +35,11 @@ export class CategoriaService {
     return this.http.delete<void>(url)
   }
 
+  update(categoria: Categoria): Observable<void>{
+    const url = `${this.baseUrl}/categorias/${categoria.id}`
+    return this.http.put<void>(url, categoria)
+  }
+
   mensagem(str: String){
     this._snack.open(`${str}`, 'OK', {
       horizontalPosition:'end',

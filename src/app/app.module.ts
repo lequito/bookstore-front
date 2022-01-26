589c85c1cd7f5bef6e4047aaaf9615209993e101
lequito <=>
2022-01-26 17:32:17 -0300

    finalizando o projeto com a leitura de livros

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -28,6 +28,7 @@ import { LivroReadAllComponent } from './components/views/livro/livro-read-all/l
 import { LivroCreateComponent } from './components/views/livro/livro-create/livro-create.component';
 import { LivroUpdateComponent } from './components/views/livro/livro-update/livro-update.component';
 import { LivroDeleteComponent } from './components/views/livro/livro-delete/livro-delete.component';
+import { LivroReadComponent } from './components/views/livro/livro-read/livro-read.component';
 
 
 @NgModule({
@@ -44,7 +45,8 @@ import { LivroDeleteComponent } from './components/views/livro/livro-delete/livr
     LivroReadAllComponent,
     LivroCreateComponent,
     LivroUpdateComponent,
-    LivroDeleteComponent
+    LivroDeleteComponent,
+    LivroReadComponent
   ],
   imports: [
     BrowserModule,
